e96793dc9e56e93c04581ab633e97aabd6baff2f
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-08-20 01:00:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(x="go to the office"){
+  return `This Monday, I will ${x}.`;
+}
+function wrapAdjective(z="*"){
+  return function innerfun(y="special"){
+    return `You are ${z}${y}${z}!`;
+  }
+}
+
+const Calculator = {
+  add : function (){ return 1+3;},
+  subtract : function (){ return 1-3;},
+  multiply : function (){ return 1*3;},
+  divide : function (){ return 10/5;}
+}
+
+function actionApplyer(a,b){
+  if(b.length===0){
+    return a;
+  }
+  else {
+    b = [
+          function(a){ return a * 2 },
+          function(a){ return a + 1000},
+          function(a){ return a % 7 }
+        ]
+  }
+}
