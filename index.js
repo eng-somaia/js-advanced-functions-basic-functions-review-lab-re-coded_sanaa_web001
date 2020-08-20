31a2ba974533984210c28dd9fb3c461fb1c1b4d0
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-08-20 01:04:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,15 +18,16 @@ const Calculator = {
   divide : function (){ return 10/5;}
 }
 
-function actionApplyer(a,b){
-  if(b.length===0){
-    return a;
-  }
-  else {
-    b = [
+let arrayOfFuncs = [
           function(a){ return a * 2 },
           function(a){ return a + 1000},
           function(a){ return a % 7 }
         ]
+function actionApplyer(a,...arrayOfFuncs){
+  if(arrayOfFuncs.length===0){
+    return a;
+  }
+  else {
+    
   }
 }
